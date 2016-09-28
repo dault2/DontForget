0ce1fcc4666826858bf93f6cab387dc112632e67
Derek Ault <dault2@gmail.com>
2016-09-28 14:28:55 -0400

    Initial Commit

---
+++ DontForgetTests/DontForgetTests.swift
@@ -0,0 +1,36 @@
+//
+//  DontForgetTests.swift
+//  DontForgetTests
+//
+//  Created by Derek Ault on 9/28/16.
+//  Copyright © 2016 Derek Ault. All rights reserved.
+//
+
+import XCTest
+@testable import DontForget
+
+class DontForgetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
